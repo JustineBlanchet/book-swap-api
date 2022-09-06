7fc221f79df6da7af43ac382504b1af00e3a8e00
JustineBlanchet <blanchet.justine@laposte.net>
2022-09-06 14:15:53 +0200

    Update books informations for visited library and add cors origin

--- app/index.js
+++ app/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://o-book-front.vercel.app/'],
 }));
 
 // Configuration du moteur de rendu
